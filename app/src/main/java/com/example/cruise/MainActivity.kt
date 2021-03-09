9373b0d5791969c7ad322a973bd1aa6ed9d27bc7
Amisha0726 <Mishu@13>
2021-03-09 13:03:28 +0530

    test2

--- app/src/main/java/com/example/cruise/MainActivity.kt
+++ app/src/main/java/com/example/cruise/MainActivity.kt
@@ -1,12 +1,20 @@
-package com.example.cruise
+package com.example.androsupport
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    var loginButton: Bundle? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //test
+        loginButton = findViewById(R.id.LoginBtn)
+        loginButton.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this@MainActivity, loginActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
