f4b7025c632f912ba7d2d65a8a26f33005076f76
Tushar <tushartambi@gmail.com>
2021-03-15 16:17:06 +0530

    Errors handled.

--- app/src/main/java/com/example/cruise/LoginActivity.kt
+++ app/src/main/java/com/example/cruise/LoginActivity.kt
@@ -3,9 +3,9 @@ package com.example.cruise
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity2 : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_login)
     }
 }
