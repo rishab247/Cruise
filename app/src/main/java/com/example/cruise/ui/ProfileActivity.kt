db98b11e410ad50742715876b80aadb5adcf65bc
Tushar <tushartambi@gmail.com>
2021-03-23 16:27:34 +0530

    Profile Edit Page added.

--- app/src/main/java/com/example/cruise/ui/ProfileActivity.kt
+++ app/src/main/java/com/example/cruise/ui/ProfileActivity.kt
@@ -1,6 +1,9 @@
 package com.example.cruise.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -10,10 +13,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.example.cruise.R
 import com.google.android.material.textfield.TextInputLayout
+import com.theartofdev.edmodo.cropper.CropImage
+import de.hdodenhof.circleimageview.CircleImageView
 
 class ProfileActivity : AppCompatActivity() {
     var registerMode: Boolean = false
     val TAG = "ProfileActivity"
+
+    val RESULT_LOAD_IMAGE = 1
+    var imageuri: Uri? = null
+    var imageresponce: String? = null
+    lateinit var mProfileImage: CircleImageView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +35,7 @@ class ProfileActivity : AppCompatActivity() {
         val nameInput: TextInputLayout = findViewById(R.id.textInputLayout2)
         val mSaveButton: Button = findViewById(R.id.button)
         val mStatusInput: EditText = findViewById(R.id.status_id)
+        mProfileImage = findViewById(R.id.circleImageView2)
 
 
         uidInput.isEnabled = false
@@ -49,6 +60,30 @@ class ProfileActivity : AppCompatActivity() {
                 nameInput.isEnabled = false
                 mStatusInput.isEnabled = false
             }
+
+        }
+
+
+        mProfileImage.setOnClickListener {
+            val i = Intent(
+                Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            )
+
+            startActivityForResult(Intent.createChooser(i, "Select Picture"), RESULT_LOAD_IMAGE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK) {
+            imageuri = data!!.data
+            CropImage.activity(imageuri)
+                .setAspectRatio(1, 1)
+                .start(this)
+
+            mProfileImage.setImageURI(imageuri)
 
         }
     }
