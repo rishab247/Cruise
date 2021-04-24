7875879c188c9eb014d73415f66dd2875bc9c729
Rishab aggarwal <rishabaggarwal247@gmail.com>
2021-04-24 16:15:57 +0530

    image picker implementation start

--- app/src/main/java/com/example/cruise/Adapter/ChatFragment/MessageAdapter.kt
+++ app/src/main/java/com/example/cruise/Adapter/ChatFragment/MessageAdapter.kt
@@ -4,13 +4,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.cruise.Data.Messages
 import com.example.cruise.Data.User_Info
 import com.example.cruise.R
-import de.hdodenhof.circleimageview.CircleImageView
+
 
 class MessageAdapter(private val messageList: List<Messages>,var userInfo: User_Info,var senderInfo: User_Info,var mLastSeentime: Long) : RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
 
@@ -18,9 +18,15 @@ class MessageAdapter(private val messageList: List<Messages>,var userInfo: User_
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
         val message: Messages = mMessageList[viewType]
+
         val v: View
-        Log.e("asda", "onCreateViewHolder: "+message.mEmail )
-        if(message.mEmail.equals(userInfo.Email))
+        Log.e("asda", "onCreateViewHolder: "+message.mMessage +viewType.toString())
+
+
+
+
+
+        if(viewType==0)
             v  = LayoutInflater.from(parent.context).inflate(R.layout.item_message_send, parent, false)
         else
             v  = LayoutInflater.from(parent.context).inflate(R.layout.item_message_receive, parent, false)
@@ -52,5 +58,12 @@ class MessageAdapter(private val messageList: List<Messages>,var userInfo: User_
         }
     }
 
+    override fun getItemViewType(position: Int): Int {
 
+        val message: Messages = mMessageList[position]
+
+        if(message.mEmail.equals(userInfo.Email))
+        return 0
+        return 1
+    }
 }
