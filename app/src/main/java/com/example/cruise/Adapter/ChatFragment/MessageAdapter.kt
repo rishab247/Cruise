cb2f90fe825b0feb0b4a91fdf442dc9fa2b65bfa
Rishab aggarwal <rishabaggarwal247@gmail.com>
2021-04-25 20:28:55 +0530

    image picker image uploaded

--- app/src/main/java/com/example/cruise/Adapter/ChatFragment/MessageAdapter.kt
+++ app/src/main/java/com/example/cruise/Adapter/ChatFragment/MessageAdapter.kt
@@ -1,67 +1,163 @@
 package com.example.cruise.Adapter.ChatFragment
 
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.cruise.Data.Messages
 import com.example.cruise.Data.User_Info
 import com.example.cruise.R
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.storage.FileDownloadTask
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
+import java.io.File
 
 
-class MessageAdapter(private val messageList: List<Messages>,var userInfo: User_Info,var senderInfo: User_Info,var mLastSeentime: Long) : RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
+class MessageAdapter(
+    public var messageList: List<Messages>,
+    var userInfo: User_Info,
+    var senderInfo: User_Info,
+    var mLastSeentime: Long,
+   var context: Context
+) : RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
 
     private val mMessageList : List<Messages> = messageList
+    lateinit var storage: FirebaseStorage
 
+
+    fun set(messageLists: List<Messages>){
+        messageList = messageLists
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
-        val message: Messages = mMessageList[viewType]
 
         val v: View
-        Log.e("asda", "onCreateViewHolder: "+message.mMessage +viewType.toString())
 
+        storage = FirebaseStorage.getInstance()
 
 
 
 
         if(viewType==0)
             v  = LayoutInflater.from(parent.context).inflate(R.layout.item_message_send, parent, false)
-        else
+        else if(viewType==1)
             v  = LayoutInflater.from(parent.context).inflate(R.layout.item_message_receive, parent, false)
+        else if(viewType==2)
+            v  = LayoutInflater.from(parent.context).inflate(R.layout.item_message_image_send, parent, false)
+        else
+            v  = LayoutInflater.from(parent.context).inflate(R.layout.item_message_image_receive, parent, false)
 
-        return MessageViewHolder(v,message.mType)
+        return MessageViewHolder(v,viewType )
     }
 
     override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
         val message: Messages = mMessageList[position]
+        if(message.mType.equals("1")) {
+            Log.e(TAG, "onBindViewHolder: 122311231"  )
+            holder.messageText!!.text = message.mMessage
+        }
+        else{
+            Log.e(TAG, "onBindViewHolder: 1eeeeghhhg"  )
+//            holder.messageImage!!.setImageResource(R.drawable.bg_receive_message)
+            downloadFile(message,holder.messageImage)
+        }
 
-        holder.messageText.text = message.mMessage
 
     }
+    private fun downloadFile(  message: Messages,messageImage : ImageView?) {
 
+         var checkfile =  File(Environment.getExternalStorageDirectory().toString() +"/cruise/"+senderInfo.msgToken +"/"+ message.mLocation +".jpg")
+//        Log.e(TAG, "downloadFile: " + "message/" + checkfile.toUri()+checkfile.exists())
+
+        if(!checkfile.exists()) {
+            Log.e(TAG, "downloadFile: " + "message/" + senderInfo.msgToken)
+            val storageRef = storage.getReference("message/" + senderInfo.msgToken)
+            val islandRef = storageRef.child(message.mLocation)
+            val rootPath =
+                File(Environment.getExternalStorageDirectory(), "cruise/" + senderInfo.msgToken)
+            if (!rootPath.exists()) {
+                rootPath.mkdirs()
+            }
+
+            val localFile = File(rootPath, message.mLocation + ".jpg")
+            islandRef.getFile(localFile)
+                .addOnSuccessListener(OnSuccessListener<FileDownloadTask.TaskSnapshot?> {
+
+                    if (messageImage != null) {
+                        Log.e(
+                        "firebase ",
+                        ";local tem file created  created " + localFile.toString()+"   "+Uri.fromFile(  File(localFile.toString()))
+                    )
+                         val bitmap: Bitmap = MediaStore.Images.Media.getBitmap( context.contentResolver , checkfile.toUri())
+                        val stream = ByteArrayOutputStream()
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 40, stream)
+                        val bitmap1 = BitmapFactory.decodeByteArray(stream.toByteArray(), 0, stream.toByteArray().size)
+
+                        messageImage .setImageBitmap(  bitmap1 )
+                    }
+
+                    //  updateDb(timestamp,localFile.toString(),position);
+                }).addOnFailureListener(OnFailureListener { exception ->
+                    Log.e(
+                        "firebase ",
+                        ";local tem file not created  created $exception"
+                    )
+                })
+        }
+        else{
+//            var ss: Uri? = checkfile.toUri()
+            val bitmap: Bitmap = MediaStore.Images.Media.getBitmap( context.contentResolver , checkfile.toUri())
+             val stream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 40, stream)
+            val bitmap1 = BitmapFactory.decodeByteArray(stream.toByteArray(), 0, stream.toByteArray().size)
+
+            messageImage!!.setImageBitmap(  bitmap1 )
+
+        }
+    }
     override fun getItemCount(): Int {
         return mMessageList.size
 
     }
 
-    class MessageViewHolder(itemView: View,type:String) : RecyclerView.ViewHolder(itemView) {
-        var messageText: TextView
-//        var imageView: CircleImageView
-//        var messageImage: ImageView
+    class MessageViewHolder(itemView: View,type:Int) : RecyclerView.ViewHolder(itemView) {
+            var messageText: TextView? = null
+
+         var messageImage: ImageView? = null
 
         init {
+//            Log.e(TAG, "sdasdasd: "+message.mType+ "   "+message.mMessage )
+
+            if(type==0||type==1)
+
             messageText = itemView.findViewById(R.id.message_text_layout)
-//            imageView = itemView.findViewById(R.id.message_profile_layout)
-//            messageImage = itemView.findViewById(R.id.message_image_layout)
+             if(type==2||type==3) {
+                 Log.e(TAG, "testing: ")
+                   messageImage = itemView.findViewById(R.id.imageView_message_image)
+             }
         }
     }
 
     override fun getItemViewType(position: Int): Int {
 
         val message: Messages = mMessageList[position]
-
+        if(message.mType.equals("2")) {
+            if (message.mEmail.equals(userInfo.Email))
+                return 2
+            return 3
+        }
         if(message.mEmail.equals(userInfo.Email))
         return 0
         return 1
--- app/src/main/java/com/example/cruise/UI/Tabs/Chat/MessageActivity.kt
+++ app/src/main/java/com/example/cruise/UI/Tabs/Chat/MessageActivity.kt
@@ -4,12 +4,11 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.os.Build
+import android.net.Uri
 import android.os.Bundle
 import android.os.Message
+import android.provider.MediaStore
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -17,27 +16,19 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.anilokcun.uwmediapicker.UwMediaPicker
-import com.anilokcun.uwmediapicker.model.MediaType
-
 import com.example.cruise.Adapter.ChatFragment.MessageAdapter
 import com.example.cruise.Data.Messages
 import com.example.cruise.Data.Messageschat
 import com.example.cruise.Data.User_Info
 import com.example.cruise.R
-import com.google.android.gms.common.util.IOUtils
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-
 import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.UploadTask
-import net.alhazmy13.mediapicker.Image.ImagePicker
 import java.io.ByteArrayOutputStream
-import java.io.File
-import java.io.FileInputStream
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -51,6 +42,7 @@ class MessageActivity : AppCompatActivity() {
     lateinit var userInfo: User_Info
     lateinit var senderInfo: User_Info
     lateinit var mMessageList: ArrayList<Messages>
+    lateinit var tempmMessageList: ArrayList<Messages>
     lateinit var context: Context
     var mLastSeentime: Long = 0L
     val OPEN_MEDIA_PICKER = 1 // Request code
@@ -67,6 +59,8 @@ class MessageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatting)
         mMessageList = ArrayList()
+        tempmMessageList = ArrayList()
+
         context = this
         auth = FirebaseAuth.getInstance()
         currentuser = auth.currentUser!!
@@ -88,7 +82,16 @@ class MessageActivity : AppCompatActivity() {
         sendmessage()
 
         //getdata
+        val mAdapter = MessageAdapter(mMessageList, userInfo, senderInfo, mLastSeentime,this@MessageActivity)
 
+        val mLinearLayout = LinearLayoutManager(context)
+         mLinearLayout.scrollToPosition(mMessageList.size + 1)
+//        mChatList.setHasFixedSize(true)
+        mChatList.layoutManager = mLinearLayout
+        mChatList.setItemViewCacheSize(5)
+
+        mChatList.adapter = mAdapter
+        mChatList.scrollToPosition( mAdapter.itemCount - 1)
         var myRef = database.getReference("Public/Message/" + senderInfo.msgToken)
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onCancelled(error: DatabaseError) {
@@ -96,19 +99,17 @@ class MessageActivity : AppCompatActivity() {
             }
 
             override fun onDataChange(snapshot: DataSnapshot) {
-                mMessageList = ArrayList()
-
-
-                var msg: Message = Message()
+                mMessageList.clear()
+                 var msg: Message = Message()
                 for (snap in snapshot.children) {
                     var flag: Boolean = false
                     for (sna in snap.children) {
                         flag = true
 
-                        Log.e(
-                            "TAG",
-                            "onChildAdded:ssss " + (sna.getValue(Messages::class.java)!!).mMessage + "  " + sna.value.toString()
-                        )
+//                        Log.e(
+//                            "TAG",
+//                            "onChildAdded:ssss " + (sna.getValue(Messages::class.java)!!).mMessage + "  " + sna.value.toString()
+//                        )
 
                         mMessageList.add(sna.getValue(Messages::class.java)!!)
 
@@ -119,22 +120,18 @@ class MessageActivity : AppCompatActivity() {
 
                             mLastSeentime = snap.value.toString().toLong()
                         }
-                        Log.e(
-                            "TAG",
-                            "onChildAdded:ssss " + snap.key.toString() + "  " + snap.value.toString() + " " + mLastSeentime.toString()
-                        )
+//                        Log.e(
+//                            "TAG",
+//                            "onChildAdded:ssss " + snap.key.toString() + "  " + snap.value.toString() + " " + mLastSeentime.toString()
+//                        )
                     }
                 }
-                val mAdapter = MessageAdapter(mMessageList, userInfo, senderInfo, mLastSeentime)
-
-                val mLinearLayout = LinearLayoutManager(context)
-               // mLinearLayout.scrollToPosition(mMessageList.size + 1)
-                mChatList.setHasFixedSize(true)
-                mChatList.layoutManager = mLinearLayout
-
-
-                mChatList.adapter = mAdapter
+                             mAdapter.set(mMessageList)
                 mChatList.scrollToPosition( mAdapter.itemCount - 1)
+
+                mAdapter.notifyDataSetChanged()
+
+
                 if (mMessageList.size >= 1)
                     updatechatlist(senderInfo, mMessageList[mMessageList.size - 1].mMessage)
             }
@@ -162,20 +159,29 @@ class MessageActivity : AppCompatActivity() {
 //            startActivityForResult(intent, OPEN_MEDIA_PICKER)
 //
 
-            UwMediaPicker.with(this@MessageActivity)						// Activity or Fragment
-            .setRequestCode(REQUEST_CODE)				// Give request code, default is 0
-                    .setGalleryMode(UwMediaPicker.GalleryMode.ImageGallery) // GalleryMode: ImageGallery/VideoGallery/ImageAndVideoGallery, default is ImageGallery
-                    .setGridColumnCount(4)                                  // Grid column count, default is 3
-                    .setMaxSelectableMediaCount(1)                         // Maximum selectable media count, default is null which means infinite
-                    .setLightStatusBar(true)                                // Is llight status bar enable, default is true
-                    .enableImageCompression(true)				// Is image compression enable, default is false
-                    .setCompressionMaxWidth(1280F)				// Compressed image's max width px, default is 1280
-                    .setCompressionMaxHeight(720F)				// Compressed image's max height px, default is 720
-                    .setCompressFormat(Bitmap.CompressFormat.JPEG)		// Compressed image's format, default is JPEG
-                    .setCompressionQuality(85)				// Image compression quality, default is 85
-                    .setCompressedFileDestinationPath("/storage/emulated/0/cruise/images/")	// Compressed image file's destination path, default is Pictures Dir
-                    .open()
+//            UwMediaPicker.with(this@MessageActivity)						// Activity or Fragment
+//            .setRequestCode(REQUEST_CODE)				// Give request code, default is 0
+//                    .setGalleryMode(UwMediaPicker.GalleryMode.VideoGallery) // GalleryMode: ImageGallery/VideoGallery/ImageAndVideoGallery, default is ImageGallery
+//                    .setGridColumnCount(3)                                  // Grid column count, default is 3
+//                    .setMaxSelectableMediaCount(5)                         // Maximum selectable media count, default is null which means infinite
+//                    .setLightStatusBar(false) .open()
 
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, REQUEST_CODE)
+//            UwMediaPicker.with(this@MessageActivity)						// Activity or Fragment
+//                    .setRequestCode(REQUEST_CODE)				// Give request code, default is 0
+//                    .setGalleryMode(UwMediaPicker.GalleryMode.ImageGallery) // GalleryMode: ImageGallery/VideoGallery/ImageAndVideoGallery, default is ImageGallery
+//                    .setGridColumnCount(3)                                  // Grid column count, default is 3
+//                    .setMaxSelectableMediaCount(5)                         // Maximum selectable media count, default is null which means infinite
+//                    .setLightStatusBar(true)                                // Is llight status bar enable, default is true
+//                    .enableImageCompression(false)				// Is image compression enable, default is false
+//                    .setCompressionMaxWidth(1280F)				// Compressed image's max width px, default is 1280
+//                    .setCompressionMaxHeight(720F)				// Compressed image's max height px, default is 720
+//                    .setCompressFormat(Bitmap.CompressFormat.JPEG)		// Compressed image's format, default is JPEG
+//                    .setCompressionQuality(85)				// Image compression quality, default is 85
+//                    .setCompressedFileDestinationPath("/storage/emulated/0/cruise/images/test")	// Compressed image file's destination path, default is Pictures Dir
+//                    .open()
 //            Log.e("TAG", "onCreate: ")
 //            ImagePicker.Builder(this@MessageActivity)
 //                .mode(ImagePicker.Mode.GALLERY)
@@ -246,7 +252,7 @@ class MessageActivity : AppCompatActivity() {
 
 
     }
-    private fun sendimage(  path :String) {
+    private fun sendimage(  path :Uri) {
              val date = Date()
             updatetime()
 
@@ -258,19 +264,40 @@ class MessageActivity : AppCompatActivity() {
 
 
 
-              uploadimage(path)
 
-//                var messages: Messages = Messages();
-//                messages.mMessage = mMessageType.text.toString()
-//                messages.mType = "2"
-//
-//                //TODO: time mask  = 3000000000000
-//                messages.mTime = timeMilli
-//                messages.mEmail = userInfo.Email
-//                mMessageType.setText("")
-//                val myRef: DatabaseReference =
-//                        database.getReference("Public/Message/" + senderInfo.msgToken + "/msg")
-//                myRef.push().setValue(messages)
+                var messages: Messages = Messages();
+                messages.mMessage = mMessageType.text.toString()
+                messages.mType = "2"
+
+                //TODO: time mask  = 3000000000000
+
+                messages.mTime = timeMilli
+                messages.mEmail = userInfo.Email
+                mMessageType.setText("")
+                var myRef: DatabaseReference =  database.getReference("Public/Message/" + senderInfo.msgToken + "/msg")
+                messages.mLocation= myRef.push().key.toString()
+
+                val storageRef: StorageReference = storage.getReference()
+                val mountainsRef: StorageReference = storageRef.child("message/"+senderInfo.msgToken+"/"+messages.mLocation )
+                val bitmap: Bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, path)
+                mAddButton.setImageBitmap(bitmap)
+                val stream = ByteArrayOutputStream()
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream)
+                val byteArray: ByteArray = stream.toByteArray()
+                Log.e("TAG", "onActivityResult: ggggg "+path )
+
+                val uploadTask: UploadTask = mountainsRef.putBytes(byteArray)
+                uploadTask.addOnSuccessListener(object :  OnSuccessListener<UploadTask.TaskSnapshot> {
+                    override fun onSuccess(p0: UploadTask.TaskSnapshot?) {
+                        Log.e("TAG", "onSuccess: "  )
+                        myRef  =  database.getReference("Public/Message/" + senderInfo.msgToken + "/msg/"+messages.mLocation)
+                        myRef.setValue(messages)
+
+                    }
+
+                })
+
+
 
             }
 
@@ -278,55 +305,36 @@ class MessageActivity : AppCompatActivity() {
 
     }
 
-    private fun uploadimage(path :String) {
-        Log.e("TAG", "onActivityResult: "+path )
-        val storageRef: StorageReference = storage.getReference()
-
-        val mountainsRef: StorageReference = storageRef.child("message/"+senderInfo.msgToken+path )
-        if (Build.VERSION.SDK_INT < 29) {
-
-            val fIS = FileInputStream(File(path))
-//
-//            val bitmap: Bitmap = (imageView.getDrawable() as BitmapDrawable).getBitmap()
-//            val baos = ByteArrayOutputStream()
-//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-            val data: ByteArray = IOUtils.toByteArray(fIS)
-
-            val uploadTask: UploadTask = mountainsRef.putBytes(data)
-            uploadTask.addOnSuccessListener(object :  OnSuccessListener<UploadTask.TaskSnapshot> {
-                override fun onSuccess(p0: UploadTask.TaskSnapshot?) {
-                    Log.e("TAG", "onSuccess: "  )
-                 }
-
-            })
-
-        }else{
-
-        }
 
 
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == ImagePicker.IMAGE_PICKER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            val mPaths: List<String>? = data?.getStringArrayListExtra(ImagePicker.EXTRA_IMAGE_PATH)
-
-            if (mPaths != null) {
-                                sendimage(mPaths.get(0))
-
-            }
+        Log.e("TAG", "onActivityResult: "+ REQUEST_CODE)
 
 
-        }
-        if (data != null && resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
-            val selectedImagesPathsList = data.getStringArrayExtra(UwMediaPicker.UwMediaPickerImagesArrayKey)
-            if (selectedImagesPathsList != null) {
-                for ( i in selectedImagesPathsList){
-                    Log.e("TAG", "onActivityResult: "+i )
-                }
-            }
+         if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
+             mAddButton.setImageURI(data?.data)
+             val imageUri: Uri? = data!!.data
+            if(imageUri!=null)
+             sendimage(imageUri)
+
          }
+
+//        if (data != null && resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
+//            var selectedImagesPathsList = data.getStringArrayExtra(UwMediaPicker.UwMediaPickerImagesArrayKey)
+//            if (selectedImagesPathsList != null) {
+//                for ( i in selectedImagesPathsList){
+//                    Log.e("TAG", "onActivityResult:WWWW "+i )
+//                }
+//            }
+//
+//            selectedImagesPathsList = data.getStringArrayExtra(UwMediaPicker.UwMediaPickerVideosArrayKey)
+//            if (selectedImagesPathsList != null) {
+//                for ( i in selectedImagesPathsList){
+//                    Log.e("TAG", "onActivityResult:video "+i )
+//                }
+//            }
+//         }
 
 //        if (requestCode === OPEN_MEDIA_PICKER) {
 //            // Make sure the request was successful
